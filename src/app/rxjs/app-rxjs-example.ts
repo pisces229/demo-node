de4fb0e6c2f38df8c5d8b121a7feb68f8e9b1f9e
1510019 <pisces72297@gmail.com>
2022-02-15 18:03:29 +0800

    no message

--- src/app/rxjs/app-rxjs-example.ts
+++ src/app/rxjs/app-rxjs-example.ts
@@ -1,5 +1,6 @@
-import { filter, map, of, reduce } from 'rxjs';
-export default () => {
+import { interval, take, takeUntil, timer } from 'rxjs';
+import { filter, firstValueFrom, lastValueFrom, map, of, reduce } from 'rxjs';
+export default async () => {
   const doAdd = (value: number) => value + 1;
   const doFilter = (value: number) => value % 2 === 0;
   // 聲明式
@@ -23,4 +24,10 @@ export default () => {
     }
   });
   console.log(`指令式:${result2}`);
+  // interval(2000).pipe(take(10));
+  // interval(2000).pipe(takeUntil(timer(5000)));
+  console.log(`firstValueFrom(of(1, 2, 3, 4)):${await firstValueFrom(of(1, 2, 3, 4))}`);
+  console.log(`lastValueFrom(of(1, 2, 3, 4)):${await lastValueFrom(of(1, 2, 3, 4))}`);
+  console.log(`firstValueFrom(interval(1000)):${await firstValueFrom(interval(1000))}`);
+  console.log(`lastValueFrom(interval(1000)):${await lastValueFrom(interval(1000).pipe(take(5)))}`);
 }
